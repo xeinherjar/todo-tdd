97f116a5fb5efc3e13b4eb7ee9838506a906880e
Alex Pate <proto.merkabah@gmail.com>
2015-02-10 17:23:46 -0500

    functional, start of styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,61 @@
-var TodoApp = function() {
-  this.autoID = 0;
-  this.completeCount = 0;
-  this.todoCount = 0;
-  this.todos = []; 
-  
-  this.add = function(todo) {
-    var td = new Todo(todo);
-    td.id = 'td' + this.autoID;
-    this.autoID++;
-    this.todos.push(td);
-    this.todoCount += 1;
-  };
+var TD = new TodoApp();
 
-  this.remove = function(id) {
-    var idx = _.findIndex(this.todos, { id: id });
-    this.todos[idx].hidden = true;
-    this.todoCount -= 1;
-    
-  };
+var tdTask = $('input[name="td-task"]');
+var todoForm = $('#todo-form');
+var tdList = $('#td-list');
+var tdStats = $('#td-stats');
 
-  this.toggleComplete = function(id) {
-    var idx = _.findIndex(this.todos, { id: id });
-    var complete = this.todos[idx].complete;
-    this.todos[idx].complete = !this.todos[idx].complete;
-    this.completeCount = !complete ? this.completeCount += 1 :
-                                     this.completeCount -= 1;
-  };
-  
+var updateTodo = function () {
+  var html = '';
+  for (var i = 0; i < TD.todos.length; i++) {
+    if (!TD.todos[i].hidden) {
+      var td = TD.todos[i];
+      if(td.complete) {
+        html += '<li id="' + td.id + '" class="complete">' + 
+                 td.name + '<span>x</span></li>';
+
+      } else {
+        html += '<li id="' + td.id + '">' + 
+                 td.name + '<span>x</span></li>';
+      }
+    }
+  }
+
+  var stats = "<span>" + TD.completeCount + " of " +
+              TD.todoCount + " complete </span>";
+  tdList.html(html); 
+  tdStats.html(stats);
 };
 
-var Todo = function(name) {
-  this.name = name;
-  this.complete = false;
-  this.hidden = false;
-  this.id = 'td';
-};
+todoForm.on('keypress', function(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    var taskVal = tdTask.val();
+    if (taskVal === '') { return; }
+    TD.add(taskVal);
+    tdTask.val('');
 
+    updateTodo();
+  }
+});
+
+todoForm.on('click', function(e) {
+  if(_.startsWith(e.target.id, 'td')) {
+    TD.toggleComplete(e.target.id);
+    updateTodo();
+  }
+  
+  if(e.target.localName === 'span') {
+    TD.remove(e.target.parentElement.id);
+    updateTodo();
+  }
+});
+
+$('.td-filter').on('click', function(e) {
+  $('.td-filter').removeClass('active-filter');
+  $(this).addClass('active-filter');
+
+});
+
+
+updateTodo();
