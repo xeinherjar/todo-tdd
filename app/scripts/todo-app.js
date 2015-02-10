77f3c9f7ee7faabb1182759756d7bc09ede34c27
Alex Pate <proto.merkabah@gmail.com>
2015-02-10 17:23:55 -0500

    functional, start of styles

---
+++ app/scripts/todo-app.js
@@ -0,0 +1,44 @@
+( function() {
+  
+  var TodoApp = function() {
+    this.autoID = 0;
+    this.completeCount = 0;
+    this.todoCount = 0;
+    this.todos = []; 
+    
+    this.add = function(todo) {
+      var td = new Todo(todo);
+      td.id = 'td' + this.autoID;
+      this.autoID++;
+      this.todos.push(td);
+      this.todoCount += 1;
+    };
+
+    this.remove = function(id) {
+      var idx = _.findIndex(this.todos, { id: id });
+      this.todos[idx].hidden = true;
+      this.todoCount -= 1;
+      if (this.todos[idx].complete) { this.completeCount -= 1; }
+      
+    };
+
+    this.toggleComplete = function(id) {
+      var idx = _.findIndex(this.todos, { id: id });
+      var complete = this.todos[idx].complete;
+      this.todos[idx].complete = !this.todos[idx].complete;
+      this.completeCount = !complete ? this.completeCount += 1 :
+                                       this.completeCount -= 1;
+    };
+  };
+
+  var Todo = function(name) {
+    this.name = name;
+    this.complete = false;
+    this.hidden = false;
+    this.id = 'td';
+  };
+
+  window.TodoApp = TodoApp;
+  window.Todo = Todo;
+
+}());
