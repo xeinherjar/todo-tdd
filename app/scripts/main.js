74055e183d30e3c620656c844024f86130a9b810
Alex Pate <proto.merkabah@gmail.com>
2015-02-10 20:27:46 -0500

    persist and load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,71 @@ var todoForm = $('#todo-form');
 var tdList = $('#td-list');
 var tdStats = $('#td-stats');
 
+var endpoint = 'http://tiy-atl-fe-server.herokuapp.com/collections/bdd-todo-ap';
+var persist = function () {
+  var payload = {
+    todos: TD.todos,
+    autoID: TD.autoID,
+    completeCount: TD.completeCount,
+    todoCount: TD.todoCount,
+  };
+
+  $.ajax({
+      url: endpoint,
+      dataType: 'json',
+      data: { todos: payload },
+      type: 'post',
+      success : function () { console.log("yay"); },
+      fail : function () { console.log("boo"); }
+    });
+};
+
+var saveButton = $('#save');
+var saveAllowed = false;
+saveButton.on('click', function() {
+  if (!saveAllowed) { return; }
+  persist();
+  saveAllowed = false;
+  $(this).addClass('disabled');
+});
+
+var load = function () {
+  allowedSave = false;
+  saveButton.addClass('disabled');
+  
+  $.ajax({
+    url: endpoint,
+    dataType: 'json',
+    type: 'get',
+    success : function (data) { 
+      var tmp = data[0];
+
+      //because we can't have nice things
+      TD.autoID = Number(tmp.todos.autoID);
+      TD.completeCount = Number(tmp.todos.completeCount);
+      TD.todoCount = Number(tmp.todos.todoCount);
+
+      for (var i = 0; i < tmp.todos.todos.length; i++) {
+        tmp.todos.todos[i].hidden === 'true' ? 
+          tmp.todos.todos[i].hidden = true :
+          tmp.todos.todos[i].hidden = false;
+         tmp.todos.todos[i].complete === 'true' ? 
+          tmp.todos.todos[i].complete = true :
+          tmp.todos.todos[i].complete = false;
+     }
+      TD.todos = tmp.todos.todos;
+      
+      updateTodo();
+    },
+    fail : function(data) { console.log(data); }
+  });
+};
+
+$('#load').on('click', function () {
+    load();
+});
+
+
 var updateTodo = function () {
 
   var filter = $('.active-filter').text();  
@@ -52,6 +117,8 @@ todoForm.on('keypress', function(e) {
     tdTask.val('');
 
     updateTodo();
+    allowedSave = true;
+    saveButton.removeClass('disabled');
   }
 });
 
